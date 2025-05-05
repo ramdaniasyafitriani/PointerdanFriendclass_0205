578dc8293ed70a50a6cb3455d3c9eb9ff6e9d38a
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-05-05 18:43:07 +0700

    membuat deference

--- tugas.cpp
+++ tugas.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+    // deference
+    int a = 10;
+    int b = 6;
+    int y = 20;
+    int *c = &a;
+    cout << "Alamat memori : " << c << endl;
+    cout << "Nilai dari C : " << *c << endl;
+    c = &y;
+    cout << "Alamat memori : " << c << endl;
+    cout << "Nilai dari C : " << *c << endl;
+
+}
